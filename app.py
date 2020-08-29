20c9d44f6170d647cf2416a191c2a7c96f76943e
liuweiyin-hg <liuwyok@sina.com>
2020-08-29 20:20:59 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,32 @@
+import time
+
+import redis
+from flask import Flask
+
+app = Flask(__name__)
+# cache = redis.Redis(host='redis', port=6379)
+
+
+# def get_hit_count():
+#     retries = 5
+#     while True:
+#         try:
+#             return cache.incr('hits')
+#         except redis.exceptions.ConnectionError as exc:
+#             if retries == 0:
+#                 raise exc
+#             retries -= 1
+#             time.sleep(0.5)
+
+
+@app.route('/')
+def hello():
+    # count = get_hit_count()
+    return 'Hello World! Flask\n'
+    # return 'Hello World! I have been seen {} times.\n'.format(count)
+
+
+if __name__ == '__main__':
+    # app.run(host, port, debug, options)
+    # 默认值：host="127.0.0.1", port=5000, debug=False
+    app.run(host="0.0.0.0", port=8089)
