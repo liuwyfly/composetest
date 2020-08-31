597e121a4fac717ff7405bf70cb86259b7bcfd97
liuweiyin-hg <liuwyok@sina.com>
2020-09-01 02:15:41 +0800

    update - 1

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ ENV FLASK_APP app.py
 ENV FLASK_RUN_HOST 0.0.0.0
 RUN apk add --no-cache gcc musl-dev linux-headers
 COPY requirements.txt requirements.txt
-RUN pip install -r requirements.txt
+RUN pip install -r requirements.txt -i http://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com
 EXPOSE 5000
 COPY . .
 CMD ["flask", "run"]
--- app.py
+++ app.py
@@ -4,26 +4,26 @@ import redis
 from flask import Flask
 
 app = Flask(__name__)
-# cache = redis.Redis(host='redis', port=6379)
+cache = redis.Redis(host='redis', port=6379)
 
 
-# def get_hit_count():
-#     retries = 5
-#     while True:
-#         try:
-#             return cache.incr('hits')
-#         except redis.exceptions.ConnectionError as exc:
-#             if retries == 0:
-#                 raise exc
-#             retries -= 1
-#             time.sleep(0.5)
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
 
 
 @app.route('/')
 def hello():
-    # count = get_hit_count()
-    return 'Hello World! Flask\n'
-    # return 'Hello World! I have been seen {} times.\n'.format(count)
+    # return 'Hello World! Flask\n'
+    count = get_hit_count()
+    return 'Hello World! I have been seen {} times.\n'.format(count)
 
 
 if __name__ == '__main__':
